951cbbd4d6e5776ade0dd293e66ef6705ebc8211
maggie <maggieww1111@gmail.com>
2017-10-12 15:11:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/ordersysetm/Sec.java
@@ -0,0 +1,13 @@
+package com.example.student.ordersysetm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sec extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sec);
+    }
+}
